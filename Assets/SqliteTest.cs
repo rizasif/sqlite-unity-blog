4eca10faeb8e2e8554793584ab54f4ae46b1d714
Rizwan Asif <rizasif@gmail.com>
2018-07-16 19:42:27 +0500

    initial commit

---
+++ Assets/SqliteTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class SqliteTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string connection = "URI=file:" + Application.persistentDataPath + "/" + "My_Database";
+		IDbConnection dbcon = new SqliteConnection(connection);
+		dbcon.Open();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
